8a7f93f775c1df1b9293f64f9c464225ae490e47
diegoxkaf <diegoxkaf@hotmail.com>
2026-02-11 23:35:21 -0300

    correccion_bugs

--- js/utils/logger.js
+++ js/utils/logger.js
@@ -7,7 +7,7 @@
  * Determina si estamos en modo debug
  * En producción, solo se mostrarán warnings y errors
  */
-const DEBUG = true; // Cambiar a false en producción o usar variable de entorno
+const DEBUG = false; // Cambiar a false en producción o usar variable de entorno
 
 /**
  * Niveles de log
